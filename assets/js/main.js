8822f04a930dee0d11d65a94d3ce2eef788ca0a8
zahid ali khan <zahidalikhan207028@gmail.com>
2024-09-27 12:41:08 +0500

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,81 @@
+function navMenu() {
+    let navBar = document.querySelector('.navbar-sticky'); 
+    let scrollTopButton = document.querySelector('#scrollup');
+
+    window.addEventListener('scroll', function () { 
+        var scroll = document.documentElement.scrollTop;
+
+        if (scroll >= 120) {
+            navBar.classList.add("navbar-sticky-moved-up");
+        } else {
+            navBar.classList.remove("navbar-sticky-moved-up");
+        }
+
+        if (scroll >= 250) {
+            navBar.classList.add("navbar-sticky-transitioned");
+            // scrollTopButton.classList.add("scrollActive"); 
+        } else {
+            navBar.classList.remove("navbar-sticky-transitioned");
+            // scrollTopButton.classList.remove("scrollActive"); 
+        }
+
+        if (scroll >= 500) {
+            navBar.classList.add("navbar-sticky-on");
+        } else {
+            navBar.classList.remove("navbar-sticky-on");
+        }
+    });
+}
+
+navMenu();
+
+
+document.addEventListener("DOMContentLoaded",()=>{
+    function counter(id, start,end, duration){
+        let obj = document.getElementById(id),
+        current = start,
+        range = end - start,
+        increment = end > start ? 1 : -1,
+        step = Math.abs(Math.floor(duration / range)),
+        timer = setInterval(()=>{
+            current += increment;
+            obj.textContent = current;
+            if(current == end){
+                clearInterval(timer)
+            }
+
+        },step);
+    }
+
+    counter("count1", 0,10,2000);
+    counter("count2", 100,20,3000);
+    counter("count3", 0,30,4000);
+    counter("count4", 0,10,2000);
+
+});
+
+
+var swiper = new Swiper('.swiper-container.app-screen', {
+    effect: 'coverflow',
+    loop: true,
+   
+    autoplay: {
+        delay: 5000,
+        disableOnInteraction: false,
+    },
+    autoplaySpeed: 3000,
+    centeredSlides: true,
+    slidesPerView: 'auto',
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+    paginationClickable: true,
+    coverflow:  {
+        rotate: 5,
+        stretch: 100,
+        depth: 150,
+        modifier: 1.5,
+        slideShadows: false,
+    }
+});
